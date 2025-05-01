bce3963583a6d3872b6a636092c7ea56eb571b96
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2025-05-01 14:14:49 +0200

    completed UI

--- main.gd
+++ main.gd
@@ -5,14 +5,23 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	print("Calling updatescore")
+	print(score)
+	print(type_string(typeof(score)))
+	print(type_string(typeof((str(score)))))
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
